9b2bb98b68d8fd17efbd9f3dc5190cc2a1869cce
Jack Dunning <JackStunning9001@gmail.com>
2020-04-09 11:09:12 -0700

    Add About page

--- BodyJournalClient/Controllers/HomeController.cs
+++ BodyJournalClient/Controllers/HomeController.cs
@@ -17,6 +17,11 @@ namespace BodyJournalClient.Controllers
       return View();
     }
 
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
     // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     // public IActionResult Error()
     // {
