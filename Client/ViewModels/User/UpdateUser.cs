83b82be936095fc76d4eb924d880e723496d0488
Andrew Philpott <andrewphilpott92@gmail.com>
2020-04-16 22:32:25 -0700

    Update gitignores.

--- Client/ViewModels/User/UpdateUser.cs
+++
@@ -1,8 +0,0 @@
-namespace BodyJournalClient.ViewModels.User
-{
-  public class UpdateUser
-  {
-    public string UserName { get; set; }
-    public string Password { get; set; }
-  }
-}
--- Client/ViewModels/User/ViewUser.cs
+++
@@ -1,8 +0,0 @@
-namespace BodyJournalClient.ViewModels.User
-{
-  public class ViewUser
-  {
-    public string UserName { get; set; }
-    public string Password { get; set; }
-  }
-}
