59ed3bfdf01630dc6dfcd091268b1f58f8b439a4
Andrew Philpott <andrewphilpott92@gmail.com>
2020-04-23 17:19:44 -0700

    Add styles.

---
+++ app/src/views/Workouts/Workout.js
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
+export default function Workout(props) {
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell component="th" scope="row">
+              Exercises
+            </TableCell>
+            key={exercise.name}
+            id={exercise.id}
+            name={exercise.name}
+            weight={exercise.weight}
+            reps={exercise.reps}
+            sets={exercise.sets}
+            intensity={exercise.intensity}
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Weight</TableCell>
+            <TableCell align="right">Reps</TableCell>
+            <TableCell align="right">Sets</TableCell>
+            <TableCell align="right">Intensity</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.exercises &&
+            props.exercises.map((exercise) => (
+              <TableRow key={exercise.id}>
+                <TableCell component="th" scope="row">
+                  {exercise.name}
+                </TableCell>
+                <TableCell align="right">{exercise.weight}</TableCell>
+                <TableCell align="right">{exercise.reps}</TableCell>
+                <TableCell align="right">{exercise.sets}</TableCell>
+                <TableCell align="right">{exercise.intensity}</TableCell>
+              </TableRow>
+            ))}
+          }
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+Workout.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  timeOfWorkout: PropTypes.string,
+  satisfaction: PropTypes.number,
+  intensityScore: PropTypes.number,
+  exercises: PropTypes.arrayOf(Exercise),
+};
